c03cfdab630277a22b59f7d476111c7182bef180
JoseAndreasW1 <c14220135@john.petra.ac.id>
2025-01-20 11:31:10 +0700

    frontEnd

--- resources/views/public/pages/home.blade.php
+++ resources/views/public/pages/home.blade.php
@@ -3,10 +3,6 @@
 @push('addon-style')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <style>
-        p.text-white * {
-            color: white !important;
-        }
-
         .swiper-slide {
             width: auto; 
         }
@@ -15,57 +11,64 @@
 @endpush
 
 @section('container')
-    <main class="w-screen relative overflow-clip ">
+    <main class="relative overflow-clip">
         {{-- 1 --}}
         <section
-            class="flex flex-col font-[600] text-3xl pt-48 tracking-widest text-center font-poppins text-[#2E3191] w-full h-[64rem] 
-             bg-cover bg-center transform  z-0"
+            class="w-full h-[64rem] flex flex-col font-[600] justify-between text-3xl pb-64 px-[5%] pt-48 tracking-widest text-center font-poppins 
+             bg-cover bg-center transform z-0"
             style="background-image: url('{{ asset('assets/images/banner.png') }}')">
-            <h1 class="mx-72 leading-relaxed">{{ $master_web->title }}</h1>
+            <h1 class="text-[#2E3191]">{{ $master_web->title }}</h1>
+            <div class="text-center w-full rounded-xl bg-[#ffffffb5] py-4">
+                <h1 class="text-lg tracking-normal">Our Clients:</h1>
+                <div class="grid grid-cols-6 w-fit gap-x-16 mx-auto">
+                    @for ($i = 0; $i < 6; $i++)
+                        <img class="h-28 " src="{{ asset('assets/images/starGroupLogo.png') }}" alt="Star Group Logo">
+                    @endfor
+                </div>
+            </div>
         </section>
         {{-- 2 --}}
-        <section class="w-[120%] h-[32rem] -mt-48 bg-white transform -rotate-6 -ms-8 relative z-10">
-            <div class="relative flex justify-between w-full h-full transform rotate-6 overflow-visible flex py-28 ps-16 pe-64">
-                <div class="bg-slate-300 flex flex-[4] items-center h-full">
-                    <img class="h-full" src="{{ asset('assets/images/starGroupLogo.png') }}" alt="">
-                    <p class="font-poppins "> Is a company group that is ready to grow
+        <section class="w-[120%] h-[45rem] -mt-32 pb-20 bg-white transform -rotate-6 -ms-8 relative z-10">
+            <div
+                class="relative flex flex-col lg:flex-row ms-9 space-y-12 lg:space-y-0 px-[5%] py-[2.5%] lg:px-[2.5%] w-screen h-full items-center transform rotate-6 overflow-clip ">
+                <div class="flex items-center ">
+                    <img class="h-56" src="{{ asset('assets/images/starGroupLogo.png') }}" alt="">
+                </div>
+                <div class="flex flex-[2] item-center ">
+                    <p class="font-poppins text-center lg:text-start "> Is a company group that is ready to grow
                         to become the best in the world.
                         work closely with our team
                         which is a big family of Binar Group.</p>
                 </div>
-                <div class="flex flex-col flex-[5] -mb-8 -mt-16 bg-slate-300">
-                    <h1 class="text-[#2E3191] capitalize font-[600] text-3xl"><span
-                            class="overline decorat decoration-[#F8B500] decoration-[6px] overline-offset-4">our
+                <div class="flex flex-col flex-[5] lg:ms-20 text-center lg:text-start">
+                    <h1 class="text-[#2E3191] capitalize font-[600] text-3xl mb-8"><span
+                            class="overline decorat decoration-[#F8B500] decoration-[6px] overline-offset-4 ">our
                             com</span>panies</h1>
-                    <div class="grid grid-cols-4 mt-8 w-fit gap-4  ">
+                    <div class="grid grid-cols-4  w-fit gap-y-2 gap-x-12  ">
                         @for ($i = 0; $i < 8; $i++)
-                            <img class="h-32" src="{{ asset('assets/images/starGroupLogo.png') }}" alt="Star Group Logo">
+                            <img class="h-40" src="{{ asset('assets/images/starGroupLogo.png') }}" alt="Star Group Logo">
                         @endfor
                     </div>
 
                 </div>
             </div>
         </section>
-        <div class="w-[120%] h-[20rem] bg-slate-200 transform rotate-6 relative -ms-12  z-20"></div>
-
+        <div class="w-[120%] h-[20rem] bg-slate-200 transform rotate-6 relative -ms-12 mt-12 z-20"></div>
 
         {{-- 3 --}}
         {{-- gambar atas --}}
         <section
             class="w-[120%] h-[55rem] -ms-16 -mt-[20rem] -rotate-6 overflow-clip relative z-30 border-t-[0.8rem] border-[#F8B500]">
-
             <!-- Background Div -->
             <div class="absolute inset-0 -z-10 transform bg-[50%_13%] rotate-[10deg] 
                 bg-cover bg-center before:content-[''] before:absolute before:inset-0 before:bg-[#00094b8a] before:rounded-md
                 -mt-40"
                 style="background-image: url('{{ asset('assets/images/bgSkyScrapper1.png') }}')">
-
-                >
             </div>
 
             <!-- Content -->
             <div class="relative w-full h-full transform rotate-3 overflow-visible flex items-center justify-center">
-                <p class="text-white font-bold text-lg">Content on Green</p>
+                <p class="text-white font-bold text-lg">Content on</p>
             </div>
         </section>
         {{-- gambar bawah --}}
@@ -81,8 +84,8 @@
         </section>
         {{-- 4 --}}
         <section
-            class="w-[140%] -ms-16 h-[28rem] bg-white transform -mt-52 relative z-50 transform -rotate-6 border-b-[0.8rem] border-[#F8B500]">
-            <div class="rotate-6 pt-16 ms-17">
+            class="w-[120%] -ms-16 h-[32rem] bg-white transform -mt-52 relative z-50 transform -rotate-6 border-b-[0.8rem] border-[#F8B500]">
+            <div class="relative w-screen h-full rotate-6 pt-16 ms-[4rem] px-[2.5%] py-[5%]">
                 <h1 id="directors-title" class="text-[#2E3191] font-[600] text-3xl text-center mb-8">
                     {{ $master_web->directors_title}}
                 </h1>
@@ -112,46 +115,53 @@
         </section>
         {{-- 5 --}}
         <section
-            class="w-[120%] h-[40rem]  
-                    bg-cover transform rotate-6 -ms-16  -mt-40 bg-[50%_100%] scale-x-[-1]
-                    relative z-40 
-                    before:content-[''] before:absolute before:inset-0 before:bg-[#00094bb8] before:z-10 before:rounded-md"
+            class="w-[120%] h-[60rem]  
+            bg-cover transform rotate-6 -ms-16 -mt-40 bg-[50%_100%] 
+            relative z-40 
+            before:content-[''] before:absolute before:inset-0 before:bg-[#00094bb8] before:z-10 before:rounded-md"
             style="background-image: url('{{ asset('assets/images/bgSkyScrapper2.png') }}')">
-
-            <div class="relative w-full h-full transform -rotate-6 overflow-visible flex p-32 ">
-                <p class="font-bold text-lg">Content on Orange</p>
+            <div
+                class="relative w-screen ms-16 px-[5%] pb-[2.5%] pt-[5%] lg:px-[2.5%] h-full transform -rotate-6 overflow-visible flex z-20">
+                <div class="flex justify-end">
+                    <h1 class="text-white capitalize font-[600] text-3xl mb-8"><span
+                            class="overline decorat decoration-[#F8B500] decoration-[6px] overline-offset-4 ">our
+                            com</span>panies</h1>
+                </div>
             </div>
         </section>
-        <div class="w-[120%] h-44 bg-slate-200 transform -rotate-6 relative -mt-52 -ms-12  z-20"></div>
+
+        <div class="w-[120%] h-44 bg-slate-200 transform -rotate-6 relative -mt-[12.5rem] -ms-12  z-20">
+        </div>
 
         {{-- 6 --}}
-        <section class="w-[120%] h-fit bg-white -ms-16 transform -rotate-6 relative z-30">
-            <div class="rotate-6 mt-20">
-                <h1 id="contact-us-title" class="text-[#2E3191] font-[600] text-3xl text-center px-[20%] mb-8">
+        <section class="w-[120%] h-[50rem] -ms-16 transform -rotate-6 relative z-50">
+            <div class="relative w-screen h-full rotate-6 pt-16 ms-[4rem] px-[10%] py-[5%] flex flex-col mx-auto justify-center items-center">
+                <h1 id="contact-us-title" class="text-[#2E3191] font-[600] text-3xl text-center mb-8">
                     {{ $master_web->contact_us_title }}
                 </h1>
+
                 <div class="flex flex-col lg:flex-row">
-                    <div class="relative w-full h-full transform overflow-visible flex-1 p-32">
+                    <div class="relative h-full transform overflow-visible flex-1 py-24 me-24 ">
                         <div class="mb-4 flex">
-                            <a href="https://www.google.com/maps?q={{ urlencode($contact->address) }}" target="_blank" rel="noopener noreferrer">
-                                <img class="w-8" src="{{ asset('assets/images/contact_us_location.png') }}">
+                            <a class="flex-shrink-0 w-8" href="https://www.google.com/maps?q={{ urlencode($contact->address) }}" target="_blank" rel="noopener noreferrer">
+                                <img class="w-full" src="{{ asset('assets/images/contact_us_location.png') }}">
                             </a>
-                            <p class="ml-8">{{ $contact->address }}</p>
+                            <p class="ml-8 ">{{ $contact->address }}</p>
                         </div>
                         <div class="mb-4 flex items-center">
-                            <a href="mailto:{{ $contact->email }}">
-                                <img class="w-8" src="{{ asset('assets/images/contact_us_gmail.png') }}">
+                            <a class="flex-shrink-0 w-8" href="mailto:{{ $contact->email }}">
+                                <img class="w-full" src="{{ asset('assets/images/contact_us_gmail.png') }}">
                             </a>
                             <p class="ml-8">{{ $contact->email }}</p>
                         </div>
                         <div class="mb-4 flex items-center">
-                            <a href="https://wa.me/62{{ $contact->phone }}">
-                                <img class="w-8" src="{{ asset('assets/images/contact_us_wa.png') }}">
+                            <a class="flex-shrink-0 w-8" href="https://wa.me/62{{ $contact->phone }}">
+                                <img class="w-full" src="{{ asset('assets/images/contact_us_wa.png') }}">
                             </a>
                             <p class="ml-8">{{ $formatted_contact_phone }}</p>
                         </div>
                     </div>
-                    <div class="flex-1 w-full lg:w-[40vw] h-[400px] lg:h-auto bg-grayPrimary overflow-hidden rounded-3xl">
+                    <div class="flex-2 w-full lg:w-[40vw] h-[400px] lg:h-auto overflow-hidden rounded-3xl">
                         @if ($contact->map)
                             <iframe
                                 class="w-full h-full"
@@ -170,7 +180,7 @@
                 </div>
             </div>
         </section>
-        <div class="w-[120%] h-[16rem] bg-slate-200 transform -rotate-6 relative -mb-20 -mt-[4.5rem] -ms-12  z-20"></div>
+        <div class="w-[120%] h-[16rem] bg-slate-200 transform -rotate-6 relative -mb-[9.5rem] -mt-[4.5rem] -ms-12  z-20"></div>
 
     </main>
 @endsection
